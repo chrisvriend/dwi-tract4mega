86a64789382bbb2b2c083d47ed3847e72ba0f089
chrisvriend <59793342+chrisvriend@users.noreply.github.com>
2026-08-25 20:22:47 +0200

    Update dwi-02b-eddyCPU_container.sh

--- dwi-02b-eddyCPU_container.sh
+++ dwi-02b-eddyCPU_container.sh
@@ -181,12 +181,6 @@ log "$BLUE" "infering sphere sampling scheme from bvecs"
 sampling=$(python ${scriptdir}/helpers/detect_sphere_sampling.py "${DWIbvecs}" 2>"${basedir}/detect_sphere_sampling.err") \
     || { log "$RED" "detect_sphere_sampling.py failed:"; cat "${basedir}/detect_sphere_sampling.err" >&2; exit 1; }
 
-if [[ $rc -ne 0 ]]; then
-    log "$RED" "detect_sphere_sampling.py exited with code ${rc}"
-    log "$RED" "stderr:"
-    cat "${basedir}/detect_sphere_sampling.err" >&2
-    exit 1
-fi
 
 if [[ "$sampling" == "HALF_SPHERE" ]]; then
     log "$YELLOW" "Detected half-sphere sampling"
